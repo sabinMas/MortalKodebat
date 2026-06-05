ea09e658ef5afabca235382fc33b6f8547188300
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-06-04 18:12:26 -0700

    WIP before merge

--- app/server.js
+++ app/server.js
@@ -1,8 +1,8 @@
 import app from './app.js';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// //read environment variables
-// dotenv.config();
+// read environment variables
+dotenv.config();
 
 const port = process.env.PORT;
 app.listen(port, () => {
